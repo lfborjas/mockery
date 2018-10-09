235da239d2e99ac01cb6d5228a4cb80494dd5384
Luis Borjas <luisfborjas@gmail.com>
2018-10-08 22:21:56 -0400

    trying to get liberator to work, not cooperating; fuck it

--- src/mockery/core.clj
+++ src/mockery/core.clj
@@ -1,15 +1,24 @@
 (ns mockery.core
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
-            [compojure.core :refer [defroutes ANY GET POST]]))
+            [compojure.core :refer [defroutes ANY GET POST]]
+            [mockery.services :as services]))
 
 (defresource home [_]
   :available-media-types ["text/html"]
   :allowed-methods [:get]
-  :handle-ok "<html>Oh hi there</html>")
+  :handle-ok "<html>Hi there, please check my <a href=\"\">README</html>")
+
+(defresource card-service [_]
+  :available-media-types ["text/xml" "application/xml"]
+  :allowed-methods [:post]
+  :handle-created (fn [ctx]
+                    (let [body (slurp (get-in ctx [:request :body]))]
+                      (services/handle-card-request body))))
 
 (defroutes app
-  (ANY "/" [] home))
+  (ANY "/" [] home)
+  (POST "/card_service" [] card-service))
 
 (def handler
   (-> app
