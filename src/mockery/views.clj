2f8fc7ca078a22bd07f1cc5d470a802692b34733
Luis Borjas <luisfborjas@gmail.com>
2018-10-08 20:34:17 -0400

    better templates: proper date and ref num

--- src/mockery/views.clj
+++ src/mockery/views.clj
@@ -1,13 +1,13 @@
 (ns mockery.views
   (:require [selmer.parser :as p]))
 
-;; TODO: use actual date fns
-(defn defaults []
-  {:ref-num "666"
-   :date "20181006"
-   :time "162000"})
+;; generates a random 6-digit numeric string
+(defn random-ref []
+  (clojure.string/join
+   (take 6 (repeatedly #(rand-int 9)))))
 
 (defn render-card-response [template params]
-  (let [updated-params (merge (defaults) params)]
+  (let [updated-params (merge {:ref-num (random-ref)}
+                              params)]
     (p/render-file (str "card_responses/" template ".xml")
                    updated-params)))
